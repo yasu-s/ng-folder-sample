771a618c0d59109c148456d9c283707b9a08d080
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-01-26 15:37:04 +0900

    prettier設定

--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 
-import { SharedModule } from '@ng-sample/shared';
 import { MENU_COMPONENTS } from '@ng-sample/menu/components';
 import { MENU_PAGES } from '@ng-sample/menu/pages';
+import { SharedModule } from '@ng-sample/shared';
+
 import { MenuRoutingModule } from './menu-routing.module';
 
 /**
